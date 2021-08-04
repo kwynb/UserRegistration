f03da054d2ae09ef780600ccabb390fdc5640149
Kwynett Bragado <kwynett.bragado@ibm.com>
2021-08-04 17:13:26 +0800

    Add files

---
+++ src/test/java/com/bragado/userregistration/UserregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bragado.userregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
